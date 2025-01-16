e93eab13b5c1659e3769c84ad60a406238743881
Yeray Almoguera González <yalmgon271@g.educaand.es>
2025-01-16 13:09:41 +0100

    Update index.php

--- dockerPHP/index.php
+++ dockerPHP/index.php
@@ -14,7 +14,6 @@ $results = $search ? array_filter($students, fn($name) => stripos($name, $search
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Buscador de Estudiantes</title>
-    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <h1>Buscador de Estudiantes</h1>
